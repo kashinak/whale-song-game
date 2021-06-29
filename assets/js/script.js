6644edf208ddbe432e0814139642e1b65b80bb72
kashinak <kashinakfx@gmail.com>
2021-06-29 23:45:26 +0000

    add new short play whale files to html and add playsound function to js to connect audio sounds to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 // Wait for DOM to load before running game//
+
+// User buttons //
+const firstBox = document.querySelector("#game-box-one");
+const secondBox = document.querySelector("#game-box-two");
+const thirdBox = document.querySelector("#game-box-three");
+const fourthBox = document.querySelector("#game-box-four");
+const play = document.querySelector("#play");
+
+
 //Click buttons and add event listeners//
 //following code snippet is from Code Institute on eventListeners in the Maths Game lesson"
 document.addEventListener("DOMContentLoaded", function () {
@@ -12,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (this.getAttribute("data-type") === "start-game") {
 
                 alert("Now let's figure out how to really start the game")
+                
 
             } else {
 
@@ -28,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // User clicks play button //
-// Game starts let n = 1; [outer for loop playing n sounds]//
+// Game starts let n = 1; [outer loop for playing n sounds]//
 // [inner loop] Play n sounds [n++] select n random sounds and highlight a box//
 // game pause for user to select choice box highlights to different color //
 /* wait or listen for click on one of four tiles (click event) 
@@ -39,6 +49,30 @@ on correct or incorrrect options */
 // option b = incorrect sequence box turns red ans alert and sound 'game over' and go back to line 30//
 
 // 32-39 plays through when user selects correct sequence //
+
+// sound plays when user clicks game box //
+
+// play button sound effects //
+
+function playSound1(event) {
+    document.getElementById('beluga-sound').play();
+};
+
+function playSound2(event) {
+    
+    document.getElementById('orca-sound').play();
+
+};
+
+function playSound3() {
+    document.getElementById('rightwhale-sound').play();
+};
+
+function playSound4() {
+    document.getElementById('spermwhale-sound').play();
+};
+
+
 
 
 
