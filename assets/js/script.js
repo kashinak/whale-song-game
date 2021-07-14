6f0a8d7e0da6f8f3ffb64b88192efbe5a12a1d78
kashinak <kashinakfx@gmail.com>
2021-07-14 19:52:30 +0000

    add event listener to start button to enable new function startGame when start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,19 @@
 let round = 0;
 let score = 0;
 let highScore = [];
-const gameSequence = [];
+const sequence = [];
 const playerSequence = [];
 const startButton = document.getElementById("start");
+
+//upon clicking startButton, game console clears and new game sequence is played//
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    $("#round-text")[0].reset();
+    $("#score-text")[0].reset();
+    document.getElementById("start").innerHTML = "Start Game!";
+    document.getElementById("start").innerHTML = "";
+    gameSequence();
+};
+
